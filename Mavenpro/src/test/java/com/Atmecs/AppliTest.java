6fbe540e8c02e9fad35e778b7541a9608ae4be39
SubhasriNatarajan <subinatraj@gmail.com>
2019-06-12 06:31:39 -0700

    Basic

---
+++ Mavenpro/src/test/java/com/Atmecs/AppliTest.java
@@ -0,0 +1,14 @@
+package com.Atmecs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppliTest {
+
+	@Test
+	public void test() {
+		System.out.println("Test case verified");
+	}
+
+}
